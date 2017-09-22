65cbcb1cc52e0c013975f4ef7506c4eebf7c0b08
Acheron <acheron.igor.b@gmail.com>
2017-09-22 13:26:32 +0300

    logging improvements

--- wd_agent/agent_v2.py
+++ wd_agent/agent_v2.py
@@ -2,6 +2,7 @@ import argparse
 import configparser
 from collections import OrderedDict
 import datetime
+import logging
 import os
 from os.path import join
 import re
@@ -15,6 +16,11 @@ try:
 except ImportError:
     from scandir import scandir  # use scandir PyPI module on Python < 3.5
 
+try:
+    from typing import Any
+except ImportError:
+    Any = object
+
 import keyring
 import pathlib
 from msvcrt import getch
@@ -24,22 +30,13 @@ from agent_base import AgentBase
 import python_webdav.client
 from Pycrypt.Crypto import RSAcrypt, AEScrypt
 
+# Basic constants
 BASE_DIR = os.path.dirname(sys.argv[0])
 CONFIG_FILE_NAME = '.cfg'
 TEMP_FILE_EXTENSION = '.tmp'
 ENCRYPT_FOLDER_NAME = 'encrypted'
 
-# FILES_LOCATION = None
-# UPLOAD_URI = None
-# UPLOAD_DIRECTORY = '/'
-# SAVE_STRUCTURE = False
-# ONLY_LAST = True
-# UPLOAD = False
-# DELETE_ORIGIN = False
-# DELETE_ENCRYPTED = False
-# ENCRYPT = True
-# FORCE_ERRORS = False
-
+# Encryption constants
 HEADER_SIZE = 128
 READ_CHUNK_SIZE = 16384
 
@@ -72,11 +69,19 @@ class Agent(AgentBase):
         self.STAGES = OrderedDict([ ('credentials', True),
                                     ('encrypt', self.encrypt),
                                     ('upload', self.upload),
-                                    ('delete', self.delete_origin or self.delete_encrypted)
+                                    ('delete', self.delete_origin or self.delete_encrypted),
         ])
 
-
-    def run(self):
+    def run(self) -> (bool, Any):
+        """
+        This method starts the pipeline of operations specified by Agent configuration.
+        Each operation is isolated - all manipulations are performed with i/o or system
+        but not between other methods.
+        The only way methods can communicate with each other - return values.
+        Each stage method must accept previous method evaluation result as first argument.
+        No other type of connection is reasonable.
+        :return: (bool, Any), True if all stages finished with success, else False; last stage result
+        """
         result = None
         for stage, active in self.STAGES.items():
             if active:
@@ -84,11 +89,22 @@ class Agent(AgentBase):
                 try:
                     method = getattr(self, method_name)
                     result = method(result)
-                except:
-                    assert False, 'Implement proper exception handling'
+                except Exception as err:
+                    logger.error('Program now exit because method "{}" raised error: {}.'.format(method_name, err))
+                    sys.exit(-1)
+                else:
+                    logger.info('stage "{}" completed successfully'.format(stage))
+        return True, result
 
     def stage_credentials(self, *args):
-        pass
+        logger.info('credentials stage started')
+        master_key_path = join(BASE_DIR, 'master.key')
+        public_key_path = join(BASE_DIR, 'pubkey.der')
+
+        if not os.path.exists(master_key_path):
+            logger.warning('unable to locate master key file')
+        elif not os.path.exists(public_key_path):
+            logger.warning('unable to locate public key file.')
 
     def stage_encrypt(self, *args):
         pass
@@ -100,5 +116,24 @@ class Agent(AgentBase):
         pass
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    log_formatter = logging.Formatter('[%(asctime)s](App: %(name)s)<Level: %(levelname)s>: %(message)s')
+
+    # log file handler
+    file_handler = logging.FileHandler('dev_log.log')
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(log_formatter)
+
+    # console output handler
+    con_handler = logging.StreamHandler()
+    con_handler.setLevel(logging.INFO)
+    con_handler.setFormatter(log_formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(con_handler)
+
     a = Agent()
     a.run()
